2f7c6c4b090d76abf6000ff915916d6ba5b88ea8
Yumiko <wakinghira73@gmail.com>
2021-06-14 10:38:37 +0900

    workのroutes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'works/index'
-  get 'works/show'
-  get 'works/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :works
 end
