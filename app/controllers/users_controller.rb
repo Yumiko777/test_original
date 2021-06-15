302a96797db0a6a8f6a7140ce6087e13077be726
Yumiko <wakinghira73@gmail.com>
2021-06-15 23:25:45 +0900

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,21 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update]
 
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
     if @user != current_user
       redirect_to user_path(current_user), alert: "不正なアクセスです!"
     end
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "ユーザー情報を更新しました!"
     else
@@ -28,5 +26,8 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
