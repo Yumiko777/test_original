a4e1c013d6563f08b7add71f5cc65c3cc2d8c7bf
Yumiko <wakinghira73@gmail.com>
2021-06-15 14:22:55 +0900

    アソシエーション

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,6 @@
 class Work < ApplicationRecord
+  belongs_to :user
+
   validates :title,  presence: true
   validates :content,  presence: true, length: { maximum: 255 }
   enum status: { 未済: 0,  済: 1 }
