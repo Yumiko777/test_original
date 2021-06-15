b5257d613e45aa6e00154f1f0179b664c3198da1
Yumiko <wakinghira73@gmail.com>
2021-06-15 22:31:09 +0900

    アクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user != current_user
       redirect_to user_path(current_user), alert: "不正なアクセスです!"
-  end
+    end
   end
 
   def update
