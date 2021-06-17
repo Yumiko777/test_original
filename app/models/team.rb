34d4b8d211a4db88c6735277db54f168e608d0e5
Yumiko <wakinghira73@gmail.com>
2021-06-17 13:39:31 +0900

    アソシエーション

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,6 @@
 class Team < ApplicationRecord
+  belongs_to :user
+
+  has_many :members, dependent: :destroy
+  has_many :member_users, through: :members, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   validates :username, presence: true
   has_many :works, dependent: :destroy
   validates :username, presence: true
+
+  has_many :teams, dependent: :destroy
+  has_many :members, dependent: :destroy
+  has_many :member_teams, through: :members, source: :team
 end
