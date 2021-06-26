931bde543622012dde40b619daa7d125d80599bc
Yumiko <wakinghira73@gmail.com>
2021-06-26 19:49:47 +0900

    kaminari

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs or /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.all.page(params[:page]).per(3)
   end
 
   # GET /blogs/1 or /blogs/1.json
