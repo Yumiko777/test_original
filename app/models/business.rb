6b70d194d4268ba1c903fe0f5d7150a6a5c37a2f
Yumiko <wakinghira73@gmail.com>
2021-07-05 21:04:20 +0900

    Add enum and validation

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,2 +1,14 @@
 class Business < ApplicationRecord
+  validates :title,  presence: true
+  validates :status, presence: true
+
+  enum status: { start: 0,  end: 1 }
+
+  def toggle_status!
+    if start?
+      end!
+    else
+      start!
+    end
+  end
 end
