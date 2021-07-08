4d6cffba4a22fed3240357dc02b24f5a31c4d216
Yumiko <wakinghira73@gmail.com>
2021-07-08 14:48:51 +0900

    トップページの画像をHeokuに反映

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module TestOriginal
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.assets.initialize_on_precompile = false
 
     config.generators do |g|
       g.test_framework :rspec,
