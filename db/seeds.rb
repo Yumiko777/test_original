83cfd6d09b2036611c75f7239eb347b6f85da7c2
Yumiko <wakinghira73@gmail.com>
2021-07-08 23:36:06 +0900

    seedデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,36 +6,52 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(
-  username: "管理者",
-  email: "admin@example.jp" ,
-  password: "password",
-  admin: true
- )
+# User.create!(
+#   username: "管理者",
+#   email: "admin@example.jp" ,
+#   password: "password",
+#   admin: true
+#  )
 
-# start_time = Faker::Date.between(from: Date.tomorrow, to: 7.days.since)
-# status = ["0","1"]
-# 10.times do |n|
-#   User.create!(
-#     username: "User_name#{n + 1}",
-#     email: "User_email#{n + 1}@sample.com" ,
-#     password: "password",
-#     admin: false
-#    )
-  # Work.create!(
-  #   title: "Work_title#{n + 1}",
-  #   content: "Work_content#{n + 1}",
-  #   start_time: start_time,
-  #   status: rand(0..1),
-  #   remarks:" Work_remarks#{n + 1}",
-  #   user_id: rand(User.first.id..User.last.id)
-  # )
-  # Team.create!(
-  #   name: "Team_name#{n + 1}",
-  #   user_id: rand(User.first.id..User.last.id)
-  # )
-  # Member.create!(
-  #   team_id: rand(Team.first.id..Team.last.id),
-  #   user_id: rand(User.first.id..User.last.id),
-  # )
-# end
+start_time = Faker::Date.between(from: Date.tomorrow, to: 7.days.since)
+status = ["0","1"]
+
+10.times do |n|
+  User.create!(
+    username: "User_name#{n + 1}",
+    email: "User_email#{n + 1}@sample.com" ,
+    password: "password",
+    admin: false
+   )
+  Work.create!(
+    title: "Work_title#{n + 1}",
+    content: "Work_content#{n + 1}",
+    start_time: start_time,
+    status: rand(0..1),
+    remarks:" Work_remarks#{n + 1}",
+    user_id: rand(User.first.id..User.last.id)
+  )
+  Team.create!(
+    name: "Team_name#{n + 1}",
+    user_id: rand(User.first.id..User.last.id)
+  )
+  Member.create!(
+    team_id: Team.last.id,
+    user_id: User.last.id
+  )
+  Blog.create!(
+    title: "Blog_title#{n + 1}",
+    content: "Blog_content#{n + 1}",
+    user_id: rand(User.first.id..User.last.id)
+  )
+  Business.create!(
+    title: "Business_title#{n + 1}",
+    status: rand(0..1),
+    user_id: rand(User.first.id..User.last.id)
+  )
+  Comment.create!(
+    blog_id: Blog.first.id,
+    user_id: User.first.id,
+    content: "Comment_content#{n + 1}"
+  )
+end
