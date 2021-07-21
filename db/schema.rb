bdd8cee44da9455c679cadc63c246e7c8ba8bd90
Yumiko <wakinghira73@gmail.com>
2021-07-21 15:04:57 +0900

    OAuth認証

--- db/schema.rb
+++ db/schema.rb
@@ -80,14 +80,22 @@ ActiveRecord::Schema.define(version: 2021_07_09_043208) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.boolean "admin"
+    t.string "provider", default: "", null: false
+    t.string "uid", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   create_table "works", force: :cascade do |t|
