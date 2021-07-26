dd47a4341d30b0aeb028159141501fb3862958f0
Yumiko <wakinghira73@gmail.com>
2021-07-26 11:25:37 +0900

    N+1問題修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def index
     @q = User.ransack(params[:q])
     @users = @q.result
-    @businesses = Business.where(user_id: @users.ids)
+    @businesses = Business.where(user_id: @users.ids).includes(:user)
   end
 
   def show
