2aa91dc529d555ea5e6d891ae82546b5ef7fca47
Yumiko <wakinghira73@gmail.com>
2021-07-29 19:49:27 +0900

    updateからcreateに変更

--- app/models/business.rb
+++ app/models/business.rb
@@ -16,28 +16,12 @@ class Business < ApplicationRecord
 
   include CommonModule
 
-  # validate :status_check
-  # def status_check
-  #   errors.add(:status, " 出勤を登録していないと退勤を入力できません") if status == 'end'
-  # end
-
-  # validate :created_at_check
-  # def created_at_check
-  #   errors.add(:created_at, " できません") if created_at ==Date.current && created_at.count > 1
-  # end
-  # def self.today
-  #   where("created_at >= ?", Time.zone.now.beginning_of_day)
-  # end
-
-  MAX_BUSINESSES_COUNT = 2
   validate :businesses_count_must_be_within_limit, on: :create
-  private
+  MAX_BUSINESSES_COUNT = 2
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
   def businesses_count_must_be_within_limit
-    # if self.today
-    unless user.try(:admin?)
-    errors.add(:created_at, "できません: #{MAX_BUSINESSES_COUNT}") if user.businesses.count >= MAX_BUSINESSES_COUNT
+    # unless user.try(:admin?)
+    errors.add(:created_at, " 出勤状況登録は1日#{MAX_BUSINESSES_COUNT}回までです ") if user.businesses.count >= MAX_BUSINESSES_COUNT
     # && created_at.strftime("%m-%d") == Date.current.strftime("%m-%d")
     end
   end
-  end
-# end
