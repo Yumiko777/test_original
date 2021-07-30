19eda3063cc7cc87a77ac301e36efe81a4b359a9
Yumiko <wakinghira73@gmail.com>
2021-07-30 16:24:30 +0900

    テスト修正

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -3,10 +3,22 @@ require 'rails_helper'
 RSpec.describe Business, type: :model do
   before do
     @user = create(:user)
-  end
-  it 'titleに記載ありでstatusが出勤の場合バリデーションが通る' do
     user = @user
+  end
+
+  it 'titleが空の場合バリデーションが通らない' do
+    business = Business.new(title: nil, status: 0, user_id: 1)
+    expect(business).not_to be_valid
+  end
+
+  it 'titleに記載ありでstatusが出勤の場合バリデーションが通る' do
     business = Business.new(title: '成功テスト', status: 0, user_id: 1)
     expect(business).to be_valid
+  end
+
+  it 'statusが退勤の場合バリデーションが通らない' do
+    user = @user
+    business = Business.new(title: '失敗テスト', status: 1 ,user_id: 1)
+    expect(business).not_to be_valid
   end
 end
